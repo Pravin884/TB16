e8178951e1c92a9b7fc06f015693117f449693a2
Pravin <Pravin@DESKTOP-T3AHDS4>
2022-04-25 14:21:29 +0530

    By m1

--- Git2/src/B.java
+++ Git2/src/B.java
@@ -1,9 +1,16 @@
 
 public class B 
 {
+	public void m1()
+	{
+		System.out.println("m1 method");
+	}
 	public static void main(String[] args) 
 	{
 		System.out.println("hello B");
+		
+		B b=new B();
+		b.m1();
 		
 	}
 
